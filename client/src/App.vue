287484878b0391af5ddfef1f8d9deb3ebc2e391f
PoBukHaBcee <mautfat@mail.ru>
2024-08-29 19:34:50 +0300

    пиво

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,19 @@
 <script setup>
-</script>
+import { ref } from "vue"
+  const cinter = ref(0)
 
+  function increment() {
+    cinter.value++
+  }
+
+  // onMounted(()=> {console.log(cinter);})
+  
+</script>
+<!-- http://localhost:5173/ -->
 <script>
 </script>
 
 <template>
   <h1>Мы пьём пиво и работаем с Vue.js</h1>
-  <button>yes</button>
+  <button @click = "increment">Счётчик yes: {{ cinter }}</button>
 </template>
